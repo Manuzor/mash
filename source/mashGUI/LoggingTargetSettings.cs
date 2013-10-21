d2bc3d61781262f8922c4fe529279f35f292d7e2
Manuel Maier <mm198@hdm-stuttgart.de>
2013-10-21 10:14:19 +0200

    Implemented LoggingTargetSettings.logMessage.

--- source/mashGUI/LoggingTargetSettings.cs
+++ source/mashGUI/LoggingTargetSettings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using mash.Gui.Properties;
 
 namespace mash.Logging
 {
@@ -11,7 +12,8 @@ namespace mash.Logging
 
 		public void logMessage(string message, MessageLevel level = MessageLevel.Normal)
 		{
-			throw new NotImplementedException();
+			message = string.Format("{0}: {1}", DateTime.Now, message);
+			Settings.Default.GlobalLog += message;
 		}
 
 		public void Dispose()
