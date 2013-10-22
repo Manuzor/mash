f9206b58fc9363f4550d4a5c274324384608e220
Manuel Maier <mm198@hdm-stuttgart.de>
2013-10-22 17:25:59 +0200

    Logging.Target.File's default constructor now uses a default log file name which is accessible via the Logging.Target.File.DefaultLogName property.

--- source/mash.Logging/Target/File.cs
+++ source/mash.Logging/Target/File.cs
@@ -5,15 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace mash.Logging.Target
 {
 	public class File : IBase
 	{
+		public static string DefaultLogName { get; protected set; }
+
+		static File()
+		{
+			string programName = Assembly.GetExecutingAssembly().FullName;
+			DefaultLogName = string.Format("{0}.log", programName);
+		}
+
 		protected StreamWriter _fileStream;
 		public string FileName { get; set; }
 
-		public File()
+		public File() :
+			this(DefaultLogName, false)
 		{
 		}
 
