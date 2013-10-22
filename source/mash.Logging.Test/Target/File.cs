e712dce919978516be01d33054e28990dd89abe8
Manuel Maier <mm198@hdm-stuttgart.de>
2013-10-22 17:28:15 +0200

    Added test case for Logging.Target.File.

---
+++ source/mash.Logging.Test/Target/File.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mash.Logging.Test.Target
+{
+	[TestFixture]
+	public class File
+	{
+		[TestCase]
+		public void test000_BasicCreation()
+		{
+			var traget = new Logging.Target.File();
+
+		}
+	}
+}
